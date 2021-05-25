047e8c4c7bc86f571d49b787e7aa3c3f2b963e9e
IoanLT <ioan.tranole@gmail.com>
2021-05-25 14:59:45 +0100

    connecting to mongoDB and creating a transaction schema

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const {
+	getTransactions,
+	addTransactions,
+	deleteTransactions,
+} = require("../controllers/transactionController");
 
-router.get("/", (req, res) => {
-	res.send("Hello!");
-});
+router.route("/").get(getTransactions).post(addTransactions);
+
+router.route("/:id").delete(deleteTransactions);
 
 module.exports = router;
