e0235508acf7efb279de73cc120f1352f5d37acc
IoanLT <ioan.tranole@gmail.com>
2021-05-26 10:09:57 +0100

    adding a proxy to the package.json in the client, importing axios and adding a transaction action in the client global state

--- frontend/src/context/GlobalState.js
+++ frontend/src/context/GlobalState.js
@@ -1,9 +1,12 @@
 import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
+import axios from "axios";
 
 // Initial state
 const initialState = {
 	transactions: [],
+	error: null,
+	loading: true
 };
 
 // Create context
@@ -12,6 +15,23 @@ export const GlobalContext = createContext(initialState);
 // Provider component
 export const GlobalProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(AppReducer, initialState);
+
+	// Action to get a transaction from our database
+	async function getTransaction() {
+		try {
+			const res = await axios("/api/v1/transactions");
+
+			dispatch({
+				type: 'GET_TRANSACTIONS',
+				payload: res.data.data
+			})
+		} catch (err) {
+			dispatch({
+				type: "TRANSACTION_ERROR",
+				payload: err.response.data.error,
+			});
+		}
+	}
 
 	// Actions to remove a transaction
 	const deleteTransaction = (id) => {
